be4e7f2f9af6b161d57d563d461435c50ae9090b
porklander </var/mail/pi>
2022-07-05 21:57:12 +0200

    backup 2022-07-05

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -15,10 +15,10 @@ echo ""
 make clean
 #make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.ebb
 make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.ebb
-read -p "EBB firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#read -p "EBB firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 cd ~/CanBoot/scripts
 python3 flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u ba51b493cff4
-read -p "EBB firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#read -p "EBB firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 echo "Finish update EBB"
 echo ""
 
@@ -29,7 +29,7 @@ cd ~/klipper
 make clean
 #make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
 make KCONFIG=/home/pi/klipper_config/script/config.linux_mcu
-read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 sudo service klipper stop
 make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
 echo "Finish update mcu rpi"
@@ -41,10 +41,10 @@ echo ""
 make clean
 #make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_2_mcuXYEZ
 make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_2_mcuXYEZ
-read -p "mcu XYE firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#read -p "mcu XYE firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 ./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f429xx_3A0047000250315637383220-if00 btt-skr-v2
 ./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f429xx_3F0032001450305031353020-if00 btt-skr-v2
-read -p "mcu XYE firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#read -p "mcu XYE firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 echo "Finish update mcu XYE"
 echo ""
 
