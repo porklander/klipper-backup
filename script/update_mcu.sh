12741c34f990d5e51a373c10593666434d99d9d3
Porklander <ahegyi@mailbox.org>
2022-12-09 21:34:45 +0100

    Initial backup

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#yes | cp /home/pi/klipper_config/script/board_defs.py /home/pi/klipper/scripts/spi_flash/board_defs.py -iR
 yes | cp /home/pi/klipper_config/script/ercf.py /home/pi/klipper/klippy/extras/ercf.py -iR
-#yes | cp /home/pi/klipper_config/script/exclude_object.py /home/pi/klipper/klippy/extras/exclude_object.py -iR
-#yes | cp /home/pi/klipper_config/script/tuning_tower.py /home/pi/klipper/klippy/extras/tuning_tower.py -iR
-#yes | cp /home/pi/klipper_config/script/virtual_sdcard.py /home/pi/klipper/klippy/extras/virtual_sdcard.py -iR
 
 cd ~/klipper
 #git pull
+
+sudo service klipper stop
+
 
 # Update head0
 echo "Start update EBB"
@@ -30,24 +29,21 @@ make clean
 #make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
 make KCONFIG=/home/pi/klipper_config/script/config.linux_mcu
 #read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-sudo service klipper stop
 make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
 echo "Finish update mcu rpi"
 echo ""
 
-# Update mcu XYE
-echo "Start update mcu XYE"
+# Update Octopus
+echo "Start update Octopus"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_2_mcuXYEZ
-make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_2_mcuXYEZ
-#read -p "mcu XYE firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f429xx_3A0047000250315637383220-if00 btt-skr-v2
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f429xx_3F0032001450305031353020-if00 btt-skr-v2
-#read -p "mcu XYE firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-echo "Finish update mcu XYE"
+#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.octopus
+make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.octopus
+#read -p "Octopus firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+cd ~/CanBoot/scripts
+python3 flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u ae6af168e91b
+#read -p "EBB firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+echo "Finish update Octopus"
 echo ""
 
-
-sudo shutdown now
-#sudo service klipper start
+sudo service klipper start
