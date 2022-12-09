ef689143f8e24290eca1c5b8c274de856d9b0fbf
Porklander <ahegyi@mailbox.org>
2022-12-09 21:42:54 +0100

    backup 2022-12-09

--- script/git_backup.sh
+++ script/git_backup.sh
@@ -66,9 +66,7 @@ push_config(){
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-#  git push "https://voronpi:$GH_TOKEN@github.com/richardjm/voronpi-klipper-backup.git"
-  git push "git@github.com:richardjm/voronpi-klipper-backup.git"
-}
+  git push "https://github.com/porklander/klipper-backup"}
 
 grab_version
 push_config
