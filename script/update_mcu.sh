462faf3f27258727ef68401a668aa0a0ad400c85
Porklander <ahegyi@mailbox.org>
2023-01-09 16:09:34 +0100

    backup 2023-01-09

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -7,30 +7,30 @@ sudo service klipper stop
 
 
 # Update head0
-echo "Start update SB2040"
-echo ""
-make clean
-make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.sb2040
-make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.sb2040
+#echo "Start update SB2040"
+#echo ""
+#make clean
+#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.sb2040
+#make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.sb2040
 #read -p "sb2040 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-cd ~/CanBoot/scripts
-python3 flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u b3b97721329d
-#read -p "sb2040 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-echo "Finish update sb2040"
-echo ""
+#cd ~/CanBoot/scripts
+#python3 flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u b3b97721329d
+##read -p "sb2040 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#echo "Finish update sb2040"
+#echo ""
 
 # Update mcu rpi
-echo "Start update mcu rpi"
-echo ""
-cd ~/klipper
-make clean
+#echo "Start update mcu rpi"
+#echo ""
+#cd ~/klipper
+#make clean
 #make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
-make KCONFIG=/home/pi/klipper_config/script/config.linux_mcu
+#make KCONFIG=/home/pi/klipper_config/script/config.linux_mcu
 #read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-sudo service klipper stop
-make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
-echo "Finish update mcu rpi"
-echo ""
+#sudo service klipper stop
+#make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
+#echo "Finish update mcu rpi"
+#echo ""
 
 # Update Octopus
 echo "Start update Octopus"
